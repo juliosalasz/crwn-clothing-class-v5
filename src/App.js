902de4a4e06bbccd67882124bfc2fe178b9d0322
kloj00 <jscomic@gmail.com>
2022-02-14 17:42:25 -0500

    Header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,12 @@ import "./App.css";
 
 import Homepage from "./pages/HomepageComponent/homepageComponent";
 import ShopComponent from "./pages/shop/ShopComponent";
+import HeaderComponent from "./components/Header/HeaderComponent";
 
 function App() {
   return (
     <BrowserRouter>
+      <HeaderComponent />
       <Switch>
         <Route exact path="/" component={Homepage} />
         <Route path="/shop" component={ShopComponent} />
