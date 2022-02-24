10e7bc789255bd15a2c9c47f19b7b75f3c778d5b
kloj00 <jscomic@gmail.com>
2022-02-23 19:04:10 -0500

    Spinner loading Database Error

---
+++ src/redux/shop/shopActions.js
@@ -0,0 +1,39 @@
+import ShopActionTypes from "./shopTypes";
+import {
+  firestore,
+  convertCollectionSnapshotToMap,
+} from "../../firebase/Firebase.utils";
+
+export const fetchCollectionsStart = () => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_START,
+});
+
+export const fetchCollectionsSuccess = (collectionsMap) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_SUCESS,
+  payload: collectionsMap,
+});
+
+export const fetchCollectionsFailure = (errorMessage) => ({
+  type: ShopActionTypes.FETCH_COLLECTIONS_FAILURE,
+  payload: errorMessage,
+});
+
+export const fetchCollectionsStartAsync = () => {
+  return (dispatch) => {
+    const collectionRef = firestore.collection("collections");
+    dispatch(fetchCollectionsStart());
+
+    collectionRef
+      .get()
+      .then((snapshot) => {
+        const collectionsMap = convertCollectionSnapshotToMap(snapshot);
+        dispatch(fetchCollectionsSuccess(collectionsMap));
+      })
+      .catch((error) => dispatch(fetchCollectionsFailure(error.message)));
+  };
+};
+
+export const updateCollections = (collectionsMap) => ({
+  type: ShopActionTypes.UPDATE_COLLECTIONS,
+  payload: collectionsMap,
+});
